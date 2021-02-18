056a70f422b045feebae100741dac452ca64063f
Tako <73486278+tako93@users.noreply.github.com>
2021-02-18 18:13:25 +0400

    Angular

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Todo {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'first-ng-app';
   number1: number = 17;
   number2: number = 20;
   counter: number = 0;
+  currentTodo?: Todo;
 
 
+  ngOnInit() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json: Todo) => {
+        this.currentTodo = json;
+      });
+  }
 
 
   onClick(event: Event) {
@@ -20,15 +37,12 @@ export class AppComponent  {
   }
 
 
-  onIncrement(value: number) { 
+  onIncrement(value: number) {
     this.counter += value;
   }
-  onDecrement(value: number) { 
+  onDecrement(value: number) {
     this.counter += value;
   }
-  
-
-
 
 }
 
